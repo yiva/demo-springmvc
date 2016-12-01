8a742b187c1126362ecf5fc14ac64ce4504d9c24
yiva <wyhtsh@gmail.com>
2016-12-01 17:20:19 +0800

    create project

---
+++ src/main/java/org/yiva/springmvc/controller/TestAction.java
@@ -0,0 +1,14 @@
+package org.yiva.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestAction {
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "res";
+	}
+	
+}
